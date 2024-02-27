113528c5742982f959fc59874ac99a7a4c50ccad
Annguyen011 <nguyenvanan01102003@gmail.com>
2024-02-27 10:05:21 +0700

    Add move point

--- Assets/_Data/Scripts/Traps/Saw/SawTrap.cs
+++ Assets/_Data/Scripts/Traps/Saw/SawTrap.cs
@@ -4,18 +4,55 @@ using UnityEngine;
 
 namespace U1
 {
-    public class SawTrap : MonoBehaviour
+    public class SawTrap : Trap
     {
-        // Start is called before the first frame update
-        void Start()
+        [SerializeField] private bool isWorking;
+        [SerializeField] private float speed;
+        [SerializeField] private Transform[] movePoints;
+
+        private int indexPoint = 0;
+        private bool direction;
+
+        //private Animator animator;
+
+        private void Awake()
         {
-        
+            //animator = GetComponent<Animator>();
         }
 
-        // Update is called once per frame
-        void Update()
+        private void Start()
         {
-        
+            transform.position = movePoints[indexPoint].position;
+        }
+
+        private void Update()
+        {
+            //animator.SetBool("isWorking", isWorking);
+
+            if (Vector2.Distance(transform.position, movePoints[indexPoint].position) <= .2f)
+            {
+                if (direction)
+                {
+                    indexPoint++;
+                }
+                else
+                {
+                    indexPoint--;
+                }
+
+                if (indexPoint >= movePoints.Length - 1)
+                {
+                    indexPoint = movePoints.Length - 1;
+                    direction = false;
+                }
+                else if (indexPoint <= 0)
+                {
+                    indexPoint = 0;
+                    direction = true;
+                }
+
+            }
+            transform.position = Vector3.MoveTowards(transform.position, movePoints[indexPoint].position, speed * Time.deltaTime);
         }
     }
 }
